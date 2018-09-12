b5c4ba4790733f3fea3ff912fe8bed55db98dded
paulantezana <contacto@paulantezana.com>
2018-09-11 22:36:26 -0500

    new updates

--- src/routes/Monitoring/Maps/index.js
+++ src/routes/Monitoring/Maps/index.js
@@ -6,7 +6,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 const MyMapComponent = compose (
     withProps({
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWWkaV0hX1t5w7OpFWdJr0b_R2uJcC7SI&v=3.exp&libraries=geometry,drawing,places",
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBWWkaV0hX1t5w7OpFWdJr0b_R2uJcC7SI&v=3.exp&libraries=geometry,drawing,places",
         // googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `75vh` }} />,
